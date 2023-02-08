d52a9ec1548bbc338b20cf8f37f3de425ef76061
rudramehta772 <rudra.mehta@spiculestech.com>
2023-02-08 11:10:08 +0530

    gs20

---
+++ gs20/api/views.py
@@ -0,0 +1,11 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset =  Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes =[BasicAuthentication]
+    permission_classes = [IsAuthenticated]
